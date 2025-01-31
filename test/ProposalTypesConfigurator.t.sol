31aba449a93aa25c9eccb08914a65378360e1a6e
corydickson <7246942+corydickson@users.noreply.github.com>
2025-01-31 14:23:07 -0500

    Fix storage length of array upon deletion (#76)
    * Add fix to change storage length of array upon deletion
    
    * Remove console import
    
    ---------
    
    Co-authored-by: cairo <cairoeth@protonmail.com>

--- test/ProposalTypesConfigurator.t.sol
+++ test/ProposalTypesConfigurator.t.sol
@@ -21,6 +21,7 @@ contract ProposalTypesConfiguratorTest is Test {
 
     event ScopeCreated(uint8 indexed proposalTypeId, bytes24 indexed scopeKey, bytes4 selector, string description);
     event ScopeDisabled(uint8 indexed proposalTypeId, bytes24 indexed scopeKey);
+    event ScopeDeleted(uint8 indexed proposalTypeId, bytes24 indexed scopeKey);
 
     /*//////////////////////////////////////////////////////////////
                                 STORAGE
@@ -531,6 +532,23 @@ contract DisableScope is ProposalTypesConfiguratorTest {
         vm.expectEmit();
         emit ScopeDisabled(0, scopeKey);
         proposalTypesConfigurator.disableScope(0, scopeKey, 0);
+    }
+}
+
+contract DeleteScope is ProposalTypesConfiguratorTest {
+    function testFuzz_DeleteScope(uint256 _actorSeed) public {
+        vm.startPrank(_adminOrTimelock(_actorSeed));
+        bytes32 txTypeHash = keccak256("transfer(address,address,uint256)");
+        address contractAddress = makeAddr("contract");
+        bytes24 scopeKey = _pack(contractAddress, bytes4(txTypeHash));
+
+        assertEq(proposalTypesConfigurator.assignedScopes(0, scopeKey).length, 1);
+        vm.expectEmit();
+        emit ScopeDeleted(0, scopeKey);
+        proposalTypesConfigurator.deleteScope(0, scopeKey, 0);
+        assertEq(proposalTypesConfigurator.assignedScopes(0, scopeKey).length, 0);
+
+        vm.stopPrank();
     }
 }
 
