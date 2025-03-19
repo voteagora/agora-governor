2ecbed0c9b8c0dd016a7b8cc83987b798cabc4fb
cairo <cairoeth@protonmail.com>
2025-03-19 16:36:26 +0100

    Add tests

--- src/modules/MultiToken.sol
+++ src/modules/MultiToken.sol
@@ -6,9 +6,10 @@ import {BaseHook} from "src/hooks/BaseHook.sol";
 
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {Packing} from "@openzeppelin/contracts/utils/Packing.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact security@voteagora.com
-contract MultiTokenModule is BaseHook {
+contract MultiTokenModule is BaseHook, Ownable {
     using EnumerableSet for EnumerableSet.Bytes32Set;
 
     /*//////////////////////////////////////////////////////////////
@@ -29,9 +30,6 @@ contract MultiTokenModule is BaseHook {
     /// @notice Set of tokens that are supported by the module
     EnumerableSet.Bytes32Set private tokens;
 
-    /// @notice Mapping of tokens to index in the set
-    mapping(address token => uint256 index) private tokenIndexes;
-
     /// @notice The divisor for the weights
     uint16 public constant PERCENT_DIVISOR = 10_000;
 
@@ -39,7 +37,7 @@ contract MultiTokenModule is BaseHook {
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
-    constructor(address payable _governor) BaseHook(_governor) {}
+    constructor(address payable _governor) BaseHook(_governor) Ownable(msg.sender) {}
 
     /*//////////////////////////////////////////////////////////////
                             EXTERNAL FUNCTIONS
@@ -84,36 +82,30 @@ contract MultiTokenModule is BaseHook {
     /// @param token The address of the token to add
     /// @param weight The weight of the token
     /// @param selector The selector of the function to call, which must take (address,uint256) as arguments and return a uint256
-    function addToken(address token, uint64 weight, bytes4 selector) external {
+    function addToken(address token, uint64 weight, bytes4 selector) external onlyOwner {
         if (token == address(0)) revert InvalidToken();
         if (weight == 0) revert InvalidWeight();
         if (selector == bytes4(0)) revert InvalidSelector();
-        if (tokenIndexes[token] != 0) revert TokenAlreadyExists();
+        if (_tokenExists(token)) revert TokenAlreadyExists();
 
         bytes12 subpack = Packing.pack_4_8(selector, bytes8(weight));
         bytes32 pack = Packing.pack_20_12(bytes20(token), subpack);
 
         tokens.add(pack);
-        tokenIndexes[token] = tokens.length();
     }
 
     /// @notice Remove a token from the module
     /// @param token The address of the token to remove
-    function removeToken(address token) external {
-        if (tokenIndexes[token] == 0) revert TokenDoesNotExist();
-
-        uint256 index = tokenIndexes[token];
+    function removeToken(address token) external onlyOwner {
+        uint256 index = _findIndex(token);
         tokens.remove(tokens.at(index));
-        delete tokenIndexes[token];
     }
 
     /// @notice Gets the weight of a token
     /// @param token The address of the token to get the weight of
     /// @return weight The weight of the token
     function getTokenWeight(address token) external view returns (uint64 weight) {
-        if (tokenIndexes[token] == 0) revert TokenDoesNotExist();
-
-        bytes32 value = tokens.at(tokenIndexes[token]);
+        bytes32 value = tokens.at(_findIndex(token));
         return uint64(Packing.extract_32_8(value, 24));
     }
 
@@ -121,9 +113,7 @@ contract MultiTokenModule is BaseHook {
     /// @param token The address of the token to get the selector of
     /// @return selector The selector of the token
     function getTokenSelector(address token) external view returns (bytes4 selector) {
-        if (tokenIndexes[token] == 0) revert TokenDoesNotExist();
-
-        bytes32 value = tokens.at(tokenIndexes[token]);
+        bytes32 value = tokens.at(_findIndex(token));
         return bytes4(Packing.extract_32_4(value, 20));
     }
 
@@ -131,6 +121,8 @@ contract MultiTokenModule is BaseHook {
     /// @param index The index of the token to get the address of
     /// @return token The address of the token
     function getTokenAddress(uint256 index) external view returns (address token) {
+        if (index >= tokens.length()) revert TokenDoesNotExist();
+
         bytes32 value = tokens.at(index);
         return address(Packing.extract_32_20(value, 0));
     }
@@ -158,5 +150,47 @@ contract MultiTokenModule is BaseHook {
             beforeExecute: false,
             afterExecute: false
         });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Finds the index of a token in the set
+    /// @param token The address of the token to find the index of
+    /// @return index The index of the token
+    function _findIndex(address token) internal view returns (uint256) {
+        // Get the values in the set and iterate over them
+        uint256 length = tokens.length();
+        for (uint256 i = 0; i < length; i++) {
+            // Get the value at the current index
+            bytes32 value = tokens.at(i);
+
+            // Extract the token address, selector, and weight
+            address _token = address(Packing.extract_32_20(value, 0));
+
+            if (token == _token) return i;
+        }
+
+        revert TokenDoesNotExist();
+    }
+
+    /// @notice Check if a token exists in the set
+    /// @param token The address of the token to check
+    /// @return exists True if the token exists, false otherwise
+    function _tokenExists(address token) internal view returns (bool) {
+        // Get the values in the set and iterate over them
+        uint256 length = tokens.length();
+        for (uint256 i = 0; i < length; i++) {
+            // Get the value at the current index
+            bytes32 value = tokens.at(i);
+
+            // Extract the token address, selector, and weight
+            address _token = address(Packing.extract_32_20(value, 0));
+
+            if (token == _token) return true;
+        }
+
+        return false;
     }
 }
