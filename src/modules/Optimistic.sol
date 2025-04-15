a21df4c2714197e7552515c0f853b1315d5ff731
Cory Dickson <cory.dickson@protonmail.com>
2025-04-15 14:08:11 -0400

    H-02

--- src/modules/Optimistic.sol
+++ src/modules/Optimistic.sol
@@ -122,7 +122,7 @@ contract OptimisticModule is BaseHook {
             revert InvalidParams();
         }
 
-        proposals[proposalId].governor = msg.sender;
+        proposals[proposalId].governor = sender;
         proposals[proposalId].settings = proposalSettings;
 
         return BaseHook.afterPropose.selector;
