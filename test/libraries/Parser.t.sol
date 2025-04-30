eb114d7c75c64e51cfe41df9f91a37100fd145f7
Cory Dickson <cory.dickson@protonmail.com>
2025-04-30 14:10:02 -0700

    Improve parser test

--- test/libraries/Parser.t.sol
+++ test/libraries/Parser.t.sol
@@ -8,6 +8,8 @@ import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 contract ParserTest is Test {
     using Parser for string;
 
+    error InvalidDescription();
+
     function test_parse() public pure {
         string memory description = "my description is this one#proposalTypeId=1";
         uint8 proposalTypeId = description._parseProposalTypeId();
@@ -39,5 +41,13 @@ contract ParserTest is Test {
         string memory descriptionWithData = string.concat(description, string(proposalData));
         string memory proposalDataStr = descriptionWithData._parseProposalData();
         assertEq(string(proposalData), proposalDataStr);
+    }
+
+    function test_parse_proposalData_RevertInvalidDescription() public {
+        bytes memory proposalData = abi.encode(0x1234);
+        string memory description = "my description is this one#proposalTypeId=10#malformed=";
+        string memory descriptionWithData = string.concat(description, string(proposalData));
+        vm.expectRevert(InvalidDescription.selector);
+        descriptionWithData._parseProposalData();
     }
 }
